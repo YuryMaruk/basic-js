311ad468f53cb25f3ba3c727f770cd1031f19ce1
Yury Maruk <maruk.dev@gmail.com>
2022-10-13 16:20:12 +0300

    feat: count-cats is ready

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ function countCats(matrix) {
+  return matrix.reduce((acc, elem) => elem.filter(item => item == '^^').length + acc , 0);
+ }
 
 module.exports = {
   countCats
