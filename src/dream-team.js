601b5661ebc099228f23096ccd1e13188f1f6581
Yury Maruk <maruk.dev@gmail.com>
2022-10-13 22:54:10 +0300

    feat: dream-team is ready

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function createDreamTeam(members) {
+  if(!members || members.length === 0 || typeof members.reduce !== 'function'){
+    return false;
+  } else {
+   return members.reduce((acc, elem) => (typeof elem === 'string' && elem.length !== 0) ?
+     [...acc, elem.split(' ').join('')] : acc, []).map(elem => elem[0].toUpperCase()).sort().join('');
+  }
 }
 
 module.exports = {
