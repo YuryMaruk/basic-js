4afea61a9e32ed29d94eae1bbf93e62b55c08479
Yury Maruk <maruk.dev@gmail.com>
2022-10-15 06:36:29 +0300

    fix: change the default function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,54 +15,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
- function repeater(foo, bar) {
-  if (!foo) {
-    return;
-  }
-  let str = foo;
-  let addStr = '';
-  let strSepr = '+';
-  let addSepr = '';
-  let strCount = 1;
-  let addCount = 1;
-
-  if (typeof str !== 'string') {
-    str.toString();
-  }
-
-  if (bar.addition == true && typeof bar.addition !== 'string') {
-    bar.addition.toString();
-  }
-
-  if (bar.repeatTimes) {
-    strCount = bar.repeatTimes;
-  }
-
-  if(bar.addition){
-    addStr = bar.addition;
-    addSepr = '|';
-  }
-
-  if(bar.additionRepeatTimes){
-    addCount = bar.additionRepeatTimes;
-  }
-
-  if(bar.separator) {
-    strSepr = bar.separator;
-  }
-
-  if(bar.additionSeparator) {
-    addSepr = bar.additionSeparator;
-  }
-  console.log(str)
-
-  let result = str + (addStr.repeat(addCount).match(new RegExp(`${addStr}`, 'g')).join(`${addSepr}`));
-
-  console.log(result)
-  console.log(result.repeat(strCount).match(new RegExp(`${result}`, 'g')))
-
-  return result.repeat(strCount).match(new RegExp(`${result}`, 'g')).join(`${strSepr}`);
-
+ function repeater(/* str, options */) {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
 }
 
 module.exports = {
