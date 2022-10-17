7543908c80c7bc595db9899d19c21b94f64a3142
Yury Maruk <maruk.dev@gmail.com>
2022-10-17 19:42:23 +0300

    feat: add trasform()

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,69 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!(arr instanceof Array)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+
+  if (!arr.length) {
+    return arr;
+  }
+
+  const removeNext = '--discard-next',
+    removePrev = '--discard-prev',
+    doubleNext = '--double-next',
+    doublePrev = '--double-prev',
+    cut = 'cut',
+    copy = 'copy';
+
+  let temp;
+  return arr.reduce((acc, e, i, a) => {
+    if (acc[acc.length - 1] === cut) {
+      acc.pop();
+      return acc;
+    }
+    if (acc[acc.length - 1] === copy) {
+      acc.pop();
+      acc.push(e);
+      return [...acc, e];
+    }
+    if (e === removePrev) {
+
+      if (!a[i - 1]) {
+        return acc;
+      } else if (temp === i - 1) {
+        return acc;
+      }
+      else {
+        acc.splice(-1, 1);
+        return acc;
+      }
+    } else if (e === removeNext) {
+      if (!a[i + 1]) {
+        return acc;
+      } else {
+        temp = i + 1;
+        return [...acc, cut];
+      }
+    } else if (e === doubleNext) {
+      if (!a[i + 1]) {
+        return acc;
+      } else {
+        return [...acc, copy];
+      }
+    } else if (e === doublePrev) {
+      if (!a[i - 1]) {
+        return acc;
+      } else if (temp === i - 1) {
+        return acc;
+      } else {
+        return [...acc, acc[acc.length - 1]];
+      }
+    } else {
+      return [...acc, e];
+    }
+  }, []);
 }
 
 module.exports = {
