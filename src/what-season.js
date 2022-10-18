7386115b693eb4ae9a2c5d78e04db2db43319d58
Yury Maruk <maruk.dev@gmail.com>
2022-10-18 10:46:19 +0300

    {

--- src/what-season.js
+++ src/what-season.js
@@ -11,17 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
- function getSeason(foo) {
-  if(!foo) {
+function getSeason(foo) {
+
+  if (!foo) {
     return 'Unable to determine the time of year!';
-  } else if(typeof foo.getMonth !== 'function' || Date.prototype.toString.call(new Date()) == foo.toString()) {
-    throw new Error('Invalid date!');
-  } else {
-    let m = foo.getMonth();
-    return (( m == 0 || m == 1 || m == 11) ? 'winter' : 
-    (m == 2 || m == 3 || m == 4) ? 'spring' : 
-    (m == 5 || m == 6 || m == 7) ? 'summer' : 'autumn');
   }
+
+  try {
+    foo.getTime();
+  } catch (error) {
+    throw new Error('Invalid date!');
+  }
+  let m = foo.getMonth();
+  return ((m == 0 || m == 1 || m == 11) ? 'winter' :
+    (m == 2 || m == 3 || m == 4) ? 'spring' :
+      (m == 5 || m == 6 || m == 7) ? 'summer' : 'autumn');
+
 }
 
 module.exports = {
